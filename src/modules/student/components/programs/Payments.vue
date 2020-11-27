590dc160346d02cc6349f82a0f4ea848c048e28e
Carolina Chirivi <carotarapues@gmail.com>
2020-11-26 19:33:29 -0500

    Improve structure payments of a student for a program. Create registration payment form, showed when registrar pago is clicked

--- src/modules/student/components/programs/Payments.vue
+++ src/modules/student/components/programs/Payments.vue
@@ -5,6 +5,107 @@
         light-green darken-3 
         font-weight-black"
         >Pagos Programa auxiliar enfermería. Grupo 2020 A II semestre</v-card-title>
+        <v-btn
+        dark
+        class="my-6 light-green darken-2"
+        v-if="addPay"
+        @click="addPay = !addPay"
+        >
+            Agregar Pago
+        </v-btn>
+        <v-card
+        v-if="!addPay"
+        width="400"
+        class="mx-auto my-10"
+        >
+        <v-card-title 
+        class="font-weight-black 
+                light-blue--text 
+                text--lighten-5
+                light-blue darken-3
+                justify-center"
+        >Registrar pago</v-card-title>
+        <v-card-text>
+            <v-form ref="main" @submit.prevent="createNewBook()">
+                <v-row>
+                    <v-col cols="12">
+                        <v-autocomplete
+                            v-model="values"
+                            :items="items"
+                            dense
+                            label="Elija un pago"
+                        ></v-autocomplete>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col cols="6">
+                        <v-menu
+                            ref="menu"
+                            v-model="menu"
+                            :close-on-content-click="false"
+                            transition="scale-transition"
+                            offset-y
+                            min-width="290px"
+                        >
+                            <template v-slot:activator="{ on, attrs }">
+                            <v-text-field
+                                v-model="fecha_nacimiento"
+                                label="Fecha"
+                                prepend-icon="mdi-calendar"
+                                readonly
+                                v-bind="attrs"
+                                v-on="on"
+                            ></v-text-field>
+                            </template>
+                            <v-date-picker
+                            ref="picker"
+                            v-model="fecha_nacimiento"
+                            :max="new Date().toISOString().substr(0, 10)"
+                            min="1950-01-01"
+                            @change="save"
+                            ></v-date-picker>
+                        </v-menu>
+                    </v-col>
+                    <v-col cols="6">
+                        <v-text-field label="Valor"></v-text-field>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col cols="6">
+                        <v-select
+                        :items="items"
+                        label="Forma de pago"
+                        ></v-select>
+                    </v-col>
+                    <v-col cols="6">
+                        <v-file-input
+                            accept="image/*"
+                            label="Evidencia pago"
+                        ></v-file-input>
+                    </v-col>
+                </v-row>
+            </v-form>
+        </v-card-text>
+        <v-card-actions>
+            <v-btn
+                color="blue darken-3"
+                class="mx-auto"
+                dark
+            >
+                Registrar
+            </v-btn>
+            <v-btn
+                color="blue darken-3"
+                class="mx-auto"
+                text
+                dark
+                @click="addPay = !addPay"
+            >
+                Cancelar
+            </v-btn>
+        </v-card-actions>
+        
+        </v-card>
         <v-expansion-panels 
         inset 
         :value="expanded"
@@ -230,8 +331,20 @@
 export default {
     name: 'Payments',
     data: () => ({
-        expanded: 0
-    })
+        expanded: 0,
+        addPay: true,
+        menu: false,
+    }),
+    watch: {
+      menu (val) {
+        val && setTimeout(() => (this.$refs.picker.activePicker = 'YEAR'))
+      },
+    },
+    methods: {
+      save (date) {
+        this.$refs.menu.save(date)
+      },
+    },
 }
 </script>
 
